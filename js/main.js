4c81df4881d77115bf0691c1d9cf93037a4ca00f
PatrikGjikolaj <123626448+PatrikGjikolaj@users.noreply.github.com>
2023-04-20 12:03:12 +0200

    esercizio finito

--- js/main.js
+++ js/main.js
@@ -1,22 +1,54 @@
 let carouselElement = document.getElementById("carousel");
-
-
+let slideCorrente = 0;
+let ultimaSlideConTastoNext = 4
+let slideEls = document.getElementsByClassName("slide");
+let btnBack = document.getElementById("back");
+let bntForward = document.getElementById("forward");
 let x = document.createElement("img");
-x.src = "./img/01.webp";
-x.className = "slide show";
-carouselElement.append(x);
 
-let y = ["./img/02.webp","./img/03.webp", "./img/04.webp", "./img/05.webp"]
-
-
+let y = ["./img/01.webp", "./img/02.webp","./img/03.webp", "./img/04.webp", "./img/05.webp"]
 
 for (let c = 0; c < y.length; c++) {
+    const a = y[c]
+    let e = document.createElement("img");
+    e.src = a;
+    e.className = "slide hidden";    
+    carouselElement.append(e);
 
-    y[c] = document.createElement("img");
-    y[c].src = y[c]
-    carouselElement.append(y[c]);
-    y.className = "slide hidden";
 }
 
+btnBack.addEventListener("click", function(){
 
-console.log(y);
+    if(slideCorrente>0) {
+        for (let c = 0; c < slideEls.length; c++) {
+                const slide = slideEls[c];
+
+            if(c == slideCorrente-1 ) {
+                slide.classList.remove("hidden");
+            } else {
+                slide.classList.add("hidden");
+            }
+        }
+        
+        slideCorrente--;
+    }
+});
+
+bntForward.addEventListener("click", function(){
+
+    console.log("Current Slide: ", slideCorrente);
+
+    if(slideCorrente < ultimaSlideConTastoNext) {
+        for (let c = 0; c < slideEls.length; c++) {
+            const slide = slideEls[c];
+
+            if(c == slideCorrente+1 ) {
+                slide.classList.remove("hidden");
+            } else {
+                slide.classList.add("hidden");
+            }
+        }
+        slideCorrente++;
+    }
+
+});
